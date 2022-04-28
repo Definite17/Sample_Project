4a89bc07354d4bd756f52a329335a985ffcf8c74
ashrafnawaz <ashrafnawaz17@gmail.com>
2022-04-28 14:05:23 +0530

    Some changes in directory

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # "This a Simple python-flask Project
-  
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -10,11 +11,10 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
 
 # if __name__ == "__main__":
 #     app.run(port= 5000 , debug=True)
-
 
 
 # run: (host: str | None = None, port: int | None = None, debug: bool | None = None, load_dotenv: bool = True, **options: Any) -> None
